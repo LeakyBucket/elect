a0ac35de9bc09c1a9e0e38d40aa7ca8aaf909f3c
Glen Holcomb <MentalLabor@gmail.com>
2020-07-16 12:49:18 -0600

    Switch to `flexbuffers` to keep everything in Rust
    I don't like that Cap'n Proto requires a separate compiler and doesn't
    really generate code until compile time.  I'd much prefer to have native
    typing and everything visible all the time while looking at the source.

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,7 +1,16 @@
-// Protocols need to exist for two main processes:
+extern crate ballot;
+
+use ballot::Ballot;
+use flexbuffers;
+use serde::{Deserialize, Serialize};
+use sodiumoxide::crypto::sign::ed25519::{PublicKey, Signature};
+use uuid::Uuid;
+
+// Protocols need to exist for three main processes:
 //
 //  * Registering with a Roll
 //  * Submitting a ballot
+//  * Syncing the Roll and Ballot Box
 //
 // Registering with a Roll is likely the most complex of the two.
 //  It needs to handle a challenge, key <-> ballot exchange.  Exchange
@@ -9,6 +18,11 @@
 //
 // Submitting a ballot is simpler.  It involves connecting to the
 //  ballot box and exchanging ballot <-> receipt.
+//
+// Syncing the Roll and Ballot Box really just means ensuring both
+//  have the id and public key for an Agent (the roll sends this on
+//  successful registration) as well as ensuring both sides have a
+//  a record of a ballot being cast.
 
 // Identity
 //
@@ -21,16 +35,72 @@
 // box have a copy of the public key used by that party and can verify the
 // signature on the completed ballot.
 
-// The message definitions come from Cap'n Proto. The types are defined in
-//  `messages.capnp`
-mod messages;
-
-struct Verification {
-
+#[derive(Deserialize, Serialize)]
+pub struct Receipt {
+    accepted: bool,
+    ballot_id: Uuid,
+    sub_info: SubmissionData,
 }
 
-struct Cast {
+#[derive(Deserialize, Serialize)]
+pub struct SubmissionData {
+    bbox: Uuid,
+    box_sig: Signature,
+}
 
+#[derive(Deserialize, Serialize)]
+pub struct Registration {
+    agent_id: Uuid,
+    agent_key: PublicKey,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Challenge {
+    questions: Vec<Question>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Question {
+    query: String,
+    answer: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct RegistrationResponse {
+    accepted: bool,
+    ballot: Ballot,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Submission {
+    agent_id: Uuid,
+    key: PublicKey,
+    signature: Signature,
+    ballot: Ballot,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Sync {
+    agent_id: Uuid,
+    event: SyncEvent,
+}
+
+#[derive(Deserialize, Serialize)]
+pub enum SyncEvent {
+    Registered,
+    Submitted,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct ErrorMessage {
+    kind: ErrorType,
+}
+
+#[derive(Deserialize, Serialize)]
+pub enum ErrorType {
+    UnexpectedMessage,
+    InvalidMessage,
+    BadAnswer,
 }
 
 #[cfg(test)]
